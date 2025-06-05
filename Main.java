8756eb42fb794b992827c0717ea3cd5821dbabf2
Yaseen Haider <yaseenhaiderabbas@gmail.com>
2025-06-05 20:07:06 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Student{
+    String StudentName ;
+    int StudentId;
+    String courseName;
+    int Course;
+
+
+}
